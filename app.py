12c20320bd72f9e4c9e40d9a3a95ee35da7cdbd0
Kaushal Joshi <kaushal.joshi0212@gmail.com>
2025-06-27 21:50:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        sentiment=request.form.get('Sentiment')
+        sid =  SentimentIntensityAnalyzer()
+        score =sid.polarity_scores(sentiment)
+
+        print(sentiment)
+        print(score)
+
+        if score['neg'] >score['pos'] and score['neu']:
+            return render_template("home.html",message='Negative😞 ')
+        elif score['pos'] >score['neg'] and score['neu']:
+            return render_template("home.html",message='Positive😊')
+        else:
+            return render_template("home.html",message='Neutral😐')
+        
+    return render_template("home.html")    
+
+if __name__ == '__main__':
+    app.run(debug=True , host='0.0.0.0')
+
+
